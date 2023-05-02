bdfc423ca9fcf5e859f375b4db1f1705c41cd811
Omar Hamid <omarhamid664@gmail.com>
2023-05-02 12:14:47 -0600

    Complete exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,7 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+print "Enter store name: "
+store_name = gets.chomp
+
+Store.create!(name: "#{store_name}")
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,12 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, :numericality => {greater_than_or_equal_to: 0}
+  validate :store_carries_apparel
+
+  def store_carries_apparel
+    unless ( mens_apparel || womens_apparel)
+      errors.add(:apparel_presence, "Stores must carry apparel")
+    end
+  end
 end
